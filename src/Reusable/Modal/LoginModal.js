58605cd9b7214f3aa81d5fa7924f18c202c5ffee
franklin8197 <franklin8197@gmail.com>
2022-12-10 16:31:05 +0530

    login signin

--- src/Reusable/Modal/LoginModal.js
+++ src/Reusable/Modal/LoginModal.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Grid,Container} from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -9,8 +10,8 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
-  background: 'linear-gradient(to right bottom, #FFFFFF 0%, #FFFFAC 0%, #D1B464 25%, #5d4a1f 62.5%, #5d4a1f 100%)',
+  width: 800,
+  bgcolor:"#ccc",
   border: '2px solid #000',
  borderRadius:"20px",
   boxShadow: 24,
@@ -48,12 +49,23 @@ export default function LoginModal(props) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
+          {/* <div onClick={props.onCancel}>
+          <span className='close'/>
+          </div> */}
+        <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={6} className="LoginGrid">
+          <h3 >Thanks! for registering with us</h3>
+          <img src="../../loginimage.png" alt="" className="bannergirl" />
+          </Grid>
+        <Grid item xs={12} sm={6} md={6} className="LoginGrid" sx={{mt:3}}>
         <TextField fullWidth id="outlined-basic" label="UserName" variant="outlined" sx ={{mb:3}}/>
-        <TextField fullWidth id="outlined-basic" label="Email-Id" variant="outlined" />
+        <TextField fullWidth id="outlined-basic" label="Email Id" variant="outlined" />
         <div className='LoginBtn'>
         <Button variant="contained" onClick={props.onCancel} sx={save}>Save</Button>
          <Button variant="outlined" onClick={props.onCancel} sx={cancel}>Cancel</Button>
          </div>
+        </Grid>
+        </Grid>
         </Box>
       </Modal>
     </div>
